c281e271acf55d73cdf857b7e0bb9e677a87f197
jinshuilinxi <2329135071@qq.com>
2019-12-16 15:11:37 +0800

    1.两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -23,18 +23,42 @@
 // }
 
 // 返回下标；不重复利用数组中同样的元素
+// 缺点：双层循环，时间复杂度为O(n * (n-1))
+// let twoSum = (nums, target) => {
+//   for (let i = 0; i < nums.length ; i++) {
+//     for (let j = i + 1; j < nums.length ; j++) {
+//       if (nums[i] === target - nums[j]) {
+//         return [i, j]
+//       }
+//     }
+//   }
+// }
+
+// 使用Map数据结构，单层循环，时间复杂度为O(n)
+// let twoSum = (nums, target) => {
+//   const map = new Map();
+//   for (let i = 0; i < nums.length ; i++) {
+//     const end = target - nums[i];
+//     if (map.has(end)) {
+//       return [map.get(end), i]
+//     } else {
+//       map.set(nums[i], i)
+//     }
+//   }
+// }
+
+// 将目标值与数组每一项做减法，将值和各项下标以键值对存入对象
+// 判断将要执行的下一项是否存在对象中,若在,取出下标,输出拿到的下标以及下一项的下标,得到返回值
 let twoSum = (nums, target) => {
-  for (let i = 0; i < nums.length ; i++) {
-    for (let j = i + 1; j < nums.length ; j++) {
-      if (nums[i] === target - nums[j]) {
-        return [i, j]
-      }
+  const map = {};
+  for (let i = 0 ; i < nums.length ; i++) {
+    map[target - nums[i]] = i;
+    if (nums[i + 1] in map) {
+      return [map[nums[i + 1]], i + 1];
     }
   }
 }
-
-
  
-// const nums = [1,3,5,31,3,56, 34], target = 34;
-// const index = twoSum(nums, target);
-// console.log(index)
+const nums = [1,3,5,31,3,56, 34], target = 34;
+const index = twoSum(nums, target);
+console.log(index)
---
+++ README.md
@@ -0,0 +1,2 @@
+### JS练习
+1. [两数之和](https://github.com/jinshuilinxi/js-practice/blob/master/1.%E4%B8%A4%E6%95%B0%E4%B9%8B%E5%92%8C.js)
